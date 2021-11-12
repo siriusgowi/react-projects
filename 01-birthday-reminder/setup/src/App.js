2e1e360a0f280d6c51a262af480fc534c5bdf9dc
siriushari <siriushari0@gmail.com>
2021-11-12 13:30:58 +0530

    updated list

---
+++ 01-birthday-reminder/setup/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+function App() {
+  return <main>
+    <section>
+      <h1>welcome to reactjs</h1>
+    </section>
+  </main>;
+}
+
+
+
+export default App;
