bb5559976bce5368118acaa0eb8b2d55cc070ab9
siriushari <siriushari0@gmail.com>
2021-11-12 13:48:44 +0530

    birthday-reminder project

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -2,9 +2,14 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+  const [people, setPeople] = useState(data)
   return <main>
     <section>
-      <h1>welcome to reactjs</h1>
+      <div className='container'>
+         <List people={people}/>
+           <button onClick={()=> setPeople([])} className='btn'>clear all</button>
+      </div>
+    
     </section>
   </main>;
 }
