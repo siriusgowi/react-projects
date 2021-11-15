ff368dc40f079126ca21d4f7de890b1eb3ec4a16
siriushari <siriushari0@gmail.com>
2021-11-15 20:09:01 +0530

    created tour project

--- 02-tours/setup/src/App.js
+++ 02-tours/setup/src/App.js
@@ -5,7 +5,47 @@ import Tours from './Tours'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tours-project'
 function App() {
-  return <h2>Tours Project Setup</h2>
+  
+  const [loading, setIsLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id)=>{
+    const newTours = tours.filter((tour)=> tour.id !== tour)
+    setTours(newTours)
+  }
+   
+  const fetchTours = async()=>{
+    setIsLoading(true)
+    try
+    {
+    const response = await fetch(url)
+    const tours = await response.json()
+    setIsLoading(false)
+    setTours(tours)
+    }
+    catch(error)
+    {
+      setIsLoading(false)
+      console.log(error)
+    }
+
+  }
+
+  useEffect(()=>{
+   fetchTours()
+  },[])
+
+
+  if(loading){
+    return (
+    <main>
+    <Loading/>
+    </main>
+    )
+  }
+  return <main>
+  <Tours tours={tours} removeTour={removeTour}/>
+  </main>
 }
 
 export default App
--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+const Tours = ({tours, removeTour}) => {
+  return (
+<section>
+  <div className='title'>
+    <h1>our tours</h1>
+    <div className='underline'>
+      </div>
+      {
+        tours.map((tour)=>{
+        return <Tour key={tour.id} {...tour} removeTour = {removeTour}/>
+        })
+      }
+  </div>
+</section>
+  )
+
 };
 
 export default Tours;
